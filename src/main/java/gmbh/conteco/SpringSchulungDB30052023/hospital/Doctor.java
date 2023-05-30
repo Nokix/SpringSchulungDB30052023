ac77bd50551e30fbb9db97afb707a8a5d7cb2ffe
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-30 11:46:56 +0200

    Nurse added and injected

--- src/main/java/gmbh/conteco/SpringSchulungDB30052023/hospital/Doctor.java
+++ src/main/java/gmbh/conteco/SpringSchulungDB30052023/hospital/Doctor.java
@@ -2,12 +2,15 @@ package gmbh.conteco.SpringSchulungDB30052023.hospital;
 
 public class Doctor {
     String qualification;
+    Nurse nurse;
 
-    public Doctor(String qualification) {
+    public Doctor(String qualification, Nurse nurse) {
         this.qualification = qualification;
+        this.nurse = nurse;
     }
 
     public String assist() {
-        return this.qualification + " is helping.";
+        return this.qualification + " is helping. "
+                + this.nurse.assist();
     }
 }
---
+++ src/main/java/gmbh/conteco/SpringSchulungDB30052023/hospital/Nurse.java
@@ -0,0 +1,7 @@
+package gmbh.conteco.SpringSchulungDB30052023.hospital;
+
+public class Nurse {
+    public String assist() {
+        return "Nurse is helping.";
+    }
+}
