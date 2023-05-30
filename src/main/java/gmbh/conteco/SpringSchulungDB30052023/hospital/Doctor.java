a5210b2e7c9135c635aad74c221fa23c4deb524f
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-30 13:21:07 +0200

    @Component, @Autowired, @Value in Doctor

--- src/main/java/gmbh/conteco/SpringSchulungDB30052023/hospital/Doctor.java
+++ src/main/java/gmbh/conteco/SpringSchulungDB30052023/hospital/Doctor.java
@@ -1,7 +1,15 @@
 package gmbh.conteco.SpringSchulungDB30052023.hospital;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Doctor {
+    @Value("Lisa")
     String qualification;
+
+    @Autowired
     Nurse nurse;
 
 //    public Doctor(String qualification, Nurse nurse) {
