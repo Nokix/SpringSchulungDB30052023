8eac1d7269c76abfae707cbf3e55d4dc12df77e9
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-31 16:54:12 +0200

    Random Student generator hinzugefügt + post

--- src/main/java/gmbh/conteco/SpringSchulungDB30052023/rest/controller/StudentController.java
+++ src/main/java/gmbh/conteco/SpringSchulungDB30052023/rest/controller/StudentController.java
@@ -3,10 +3,7 @@ package gmbh.conteco.SpringSchulungDB30052023.rest.controller;
 import gmbh.conteco.SpringSchulungDB30052023.rest.entitiy.Student;
 import gmbh.conteco.SpringSchulungDB30052023.rest.service.StudentService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,13 @@ public class StudentController {
     public Student saveStudent(@PathVariable String name){
         System.out.println("Es wurde eingegeben: " + name);
         return studentService.saveStudent(name);
+    }
+
+
+    @PostMapping("/students/create")
+    public List<Student> createStudents(
+            @RequestParam(value = "n", defaultValue = "5") int amount
+    ) {
+        return studentService.createStudents(amount);
     }
 }
--- src/main/java/gmbh/conteco/SpringSchulungDB30052023/rest/service/StudentService.java
+++ src/main/java/gmbh/conteco/SpringSchulungDB30052023/rest/service/StudentService.java
@@ -1,24 +1,34 @@
 package gmbh.conteco.SpringSchulungDB30052023.rest.service;
 
+import com.github.javafaker.Faker;
 import gmbh.conteco.SpringSchulungDB30052023.rest.entitiy.Student;
 import gmbh.conteco.SpringSchulungDB30052023.rest.repository.StudentListRepo;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 @AllArgsConstructor
 public class StudentService {
     StudentListRepo studentListRepo;
+    // Wie komme ich an eine Faker instanz ran? (Spring)
+    Faker faker;
 
     public List<Student> getAllStudents() {
         return studentListRepo.findAll();
     }
 
-
     public Student saveStudent(String name) {
         return studentListRepo.save(new Student(name));
+    }
+
+    public List<Student> createStudents(int amount) {
+        return Stream.generate(faker.name()::firstName)
+                .limit(amount)
+                .map(Student::new)
+                .map(studentListRepo::save)
+                .toList();
     }
 }
