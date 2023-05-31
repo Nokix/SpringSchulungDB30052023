733417c5c66dac99b3f97264d4ce22142273a08f
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-31 14:16:41 +0200

    TimeMe Aspect

--- src/main/java/gmbh/conteco/SpringSchulungDB30052023/aop/TimerAspect.java
+++ src/main/java/gmbh/conteco/SpringSchulungDB30052023/aop/TimerAspect.java
@@ -1,9 +1,26 @@
 package gmbh.conteco.SpringSchulungDB30052023.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class TimerAspect {
+    @Around("@annotation(TimeMe)")
+    public Object measureExecutionTime(
+            ProceedingJoinPoint joinPoint
+    ) throws Throwable {
+        long start = System.nanoTime();
+        Object result = joinPoint.proceed();
+        long time = System.nanoTime() - start;
+        System.out.println(
+                joinPoint.getSignature().getName()
+                + " is executed in "
+                + time
+                + " nanoseconds."
+        );
+        return result;
+    }
 }
