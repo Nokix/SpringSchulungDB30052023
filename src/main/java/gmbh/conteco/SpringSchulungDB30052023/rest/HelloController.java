441b6b67ac00b830bf7e6bcef3fac23d9ea28f69
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-31 14:40:26 +0200

    HelloController

---
+++ src/main/java/gmbh/conteco/SpringSchulungDB30052023/rest/HelloController.java
@@ -0,0 +1,12 @@
+package gmbh.conteco.SpringSchulungDB30052023.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Moin moin DB!";
+    }
+}
